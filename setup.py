e119a5586bfd6eb2b4cde49d3eabab4dd3a59182
Ryan Avery <ravery@ucsb.edu>
2018-05-08 20:03:58 -0700

    initial commit, updated cookie cut template allows for better src package management

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Analysis of continuous eddy covariance and isotope measurements to model evaporation in drylands',
+    author='Ryan Avery',
+    license='MIT',
+)
